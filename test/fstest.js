33471bc45a7e9c05b9c595df6a63e631349ae371
namwoo.kim <sky_9233@naver.com>
2020-01-06 23:14:59 +0900

    first commit

---
+++ test/fstest.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const util = require('util');
+
+fs.readFile(__dirname+'/test.json','utf-8',(err,data) => {
+    if (err) return util.error(err);
+
+    util.log('data>>',data);
+
+});
+
+
+util.log('------------------------------')
+
+const msgfile = __dirname + '/message.txt';
+const data = new Uint8Array(Buffer.from('Hello Node.js 안녕'));
+fs.writeFileSync(msgfile, data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
+
+let data2 = fs.readFileSync(msgfile,'utf-8');
+util.log('data2>>',data2);
+
+util.log('-----------------------------')
